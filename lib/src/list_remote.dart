bb1a3a6d12ebf36c109c9a32fbbaf669a17472a3
Jesus R <itsjesusr@outlook.com>
2020-12-22 11:44:20 -0600

    feat: Use official source to list remote versions

--- lib/src/list_remote.dart
+++ lib/src/list_remote.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 Future<void> listRemote() async {
-  const url =
-      'https://gist.githubusercontent.com/itsje/75ebde8ed9555ca9f68312111b2d433e/raw';
+  const url = 'https://nodejs.org/dist/index.json';
   final request = await http.get(url);
 
   if (request.statusCode != 200) {
@@ -12,11 +11,37 @@ Future<void> listRemote() async {
     return;
   }
 
-  stdout.writeln('Last 3 releases of major NodeJS versions');
-  final remoteVersions =
-      (json.decode(request.body) as List<dynamic>).cast<String>();
+  stdout.writeln('Last 3 releases of major NodeJS versions: \n');
+  final remoteVersions = (json.decode(request.body) as List<dynamic>);
+  final versions = parseIndexJSON(remoteVersions);
 
-  for (final version in remoteVersions) {
-    print(version);
+  for (final entry in versions) {
+    stdout.writeln(entry);
   }
+}
+
+List<String> parseIndexJSON(List<dynamic> indexJSON) {
+  final versionMap = <String, int>{};
+  final versionList = <String>[];
+
+  for (final entry in indexJSON) {
+    final version = entry['version'] as String;
+    final major = version.split('.')[0];
+    final isLTS = entry['lts'] == false ? false : true;
+
+    if (int.parse(major.substring(1)) < 8) {
+      break;
+    }
+
+    if (versionMap[major] == null) {
+      versionMap[major] = 1;
+    }
+
+    if (versionMap[major] <= 3) {
+      versionMap[major] += 1;
+      versionList.add(version.substring(1) + (isLTS ? ' - LTS' : ''));
+    }
+  }
+
+  return versionList;
 }
