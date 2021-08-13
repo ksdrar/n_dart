124cbb1c7af0ae09675968fd1e3c2871ca956dee
Jesus R <rr.jesus@pm.me>
2021-08-13 12:24:41 -0500

    feat: Use "args" package to parse arguments

--- bin/n_dart.dart
+++ bin/n_dart.dart
@@ -1,89 +1,13 @@
-import 'dart:io';
+import 'package:args/command_runner.dart';
+import 'package:n_dart/src/commands.dart';
 
-import 'package:n_dart/src/globals.dart';
-import 'package:n_dart/src/install_version.dart';
-import 'package:n_dart/src/list_remote.dart';
-import 'package:n_dart/src/set_as_active.dart';
-import 'package:n_dart/src/uninstall_version.dart';
-import 'package:n_dart/src/update_npm.dart';
-import 'package:path/path.dart' as path;
-
-Future<void> main(List<String> arguments) async {
-  if (arguments.isEmpty) {
-    printHelp();
-    return;
-  }
-
-  const directories = ['', '.cache', 'versions'];
-
-  for (final dir in directories) {
-    if (!Directory(path.join(home, dir)).existsSync()) {
-      Directory(path.join(home, dir)).createSync();
-    }
-  }
-
-  switch (arguments[0]) {
-    case 'i':
-    case 'install':
-      await installVersion(arguments[1]);
-      config.saveToDisk();
-      break;
-    case 'un':
-    case 'uninstall':
-      uninstallVersion(arguments[1]);
-      config.saveToDisk();
-      break;
-    case 'use':
-      if (!config.installedVersions.containsKey(arguments[1])) {
-        stdout.writeln('Version ${arguments[1]} is not installed');
-        break;
-      } else if (arguments[1] == config.activeVersion) {
-        stdout.writeln('Version ${arguments[1]} is already active');
-        break;
-      }
-
-      setAsActive(arguments[1]);
-      config.saveToDisk();
-      break;
-    case 'ls':
-    case 'list-local':
-      stdout.writeln('Installed versions:');
-      final versionsList = [
-        for (final entry in config.installedVersions.entries)
-          entry.key + (entry.value.isActive ? ' - active' : '')
-      ];
-      versionsList.sort();
-
-      for (final version in versionsList) {
-        stdout.writeln(version);
-      }
-
-      break;
-    case 'lsr':
-    case 'list-remote':
-      await listRemote();
-      break;
-    case 'update-npm':
-      await updateNPM(arguments[1]);
-      break;
-    case '--h':
-    case '--help':
-    case 'help':
-      printHelp();
-      break;
-    default:
-      stdout.writeln('Command ${arguments[0]} is not valid');
-      break;
-  }
-}
-
-void printHelp() {
-  const help = 'Usage: n_dart <command>\n\n'
-      'n_dart install <version>     install the selected NodeJS version\n'
-      'n_dart uninstall <version>   remove the selected NodeJS version\n'
-      'n_dart use <version>         change the active version to the selected one\n'
-      'n_dart list-local            list installed versions\n'
-      'n_dart list-remote           list last 3 releases of major NodeJS versions\n'
-      'n_dart update-npm <version>  update npm version of active NodeJS version\n';
-  stdout.writeln(help);
+Future<void> main(List<String> args) async {
+  CommandRunner('n-dart', 'NodeJS version manager')
+    ..addCommand(InstallCommand())
+    ..addCommand(UninstallCommand())
+    ..addCommand(UseCommand())
+    ..addCommand(ListCommand())
+    ..addCommand(ListRemoteCommand())
+    ..addCommand(ReplaceNPMCommand())
+    ..run(args);
 }
---
+++ lib/src/commands.dart
@@ -0,0 +1,134 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:n_dart/src/globals.dart';
+import 'package:n_dart/src/install_version.dart';
+import 'package:n_dart/src/list_remote.dart';
+import 'package:n_dart/src/set_as_active.dart';
+import 'package:n_dart/src/uninstall_version.dart';
+import 'package:n_dart/src/update_npm.dart';
+
+class InstallCommand extends Command {
+  @override
+  String get description => 'Installs the typed NodeJS version';
+
+  @override
+  String get invocation => 'install <version>';
+
+  @override
+  String get name => 'install';
+
+  @override
+  List<String> get aliases => ['i'];
+
+  @override
+  Future<void> run() async {
+    await installVersion(argResults!.rest[0]);
+    config.saveToDisk();
+  }
+}
+
+class UninstallCommand extends Command {
+  @override
+  String get description => 'Uninstalls the typed NodeJS version';
+
+  @override
+  String get invocation => 'uninstall <version>';
+
+  @override
+  String get name => 'uninstall';
+
+  @override
+  List<String> get aliases => ['un'];
+
+  @override
+  void run() {
+    uninstallVersion(argResults!.rest[0]);
+    config.saveToDisk();
+  }
+}
+
+class UseCommand extends Command {
+  @override
+  String get description => 'Makes the typed NodeJS version the active one';
+
+  @override
+  String get invocation => 'use <version>';
+
+  @override
+  String get name => 'use';
+
+  @override
+  void run() {
+    if (!config.installedVersions.containsKey(argResults!.rest[0])) {
+      stdout.writeln('Version ${argResults!.rest[0]} is not installed');
+      return;
+    } else if (argResults!.rest[0] == config.activeVersion) {
+      stdout.writeln('Version ${argResults!.rest[0]} is already active');
+      return;
+    }
+
+    setAsActive(argResults!.rest[0]);
+    config.saveToDisk();
+  }
+}
+
+class ListCommand extends Command {
+  @override
+  String get description => 'Outputs a list of the installed NodeJS versions';
+
+  @override
+  String get name => 'list';
+
+  @override
+  List<String> get aliases => ['ls'];
+
+  @override
+  void run() {
+    stdout.writeln('Installed versions:');
+    final versionsList = [
+      for (final entry in config.installedVersions.entries)
+        entry.key + (entry.value.isActive ? ' - active' : '')
+    ];
+    versionsList.sort();
+
+    for (final version in versionsList) {
+      stdout.writeln(version);
+    }
+  }
+}
+
+class ListRemoteCommand extends Command {
+  @override
+  String get description => 'Outputs a list of the available NodeJS versions';
+
+  @override
+  String get name => 'list-remote';
+
+  @override
+  List<String> get aliases => ['lsr'];
+
+  @override
+  Future<void> run() async {
+    await listRemote();
+  }
+}
+
+class ReplaceNPMCommand extends Command {
+  @override
+  String get description => 'Replaces the installed npm version with the one typed';
+
+  @override
+  String get invocation => 'replace-npm <version>';
+
+  @override
+  String get name => 'replace-npm';
+
+  @override
+  List<String> get aliases => ['r-npm'];
+
+  @override
+  Future<void> run() async {
+    await updateNPM(argResults!.rest[1]);
+  }
+}
