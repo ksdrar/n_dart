632e6b6240188969ab70741abedd168721527a54
Jesus R <rr.jesus@pm.me>
2021-08-16 19:20:52 -0500

    imp: Handle UsageException for missing commands

--- bin/n_dart.dart
+++ bin/n_dart.dart
@@ -1,13 +1,20 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:n_dart/src/commands.dart';
 
-void main(List<String> args) {
-  CommandRunner('n-dart', 'NodeJS version manager')
+Future<void> main(List<String> args) async {
+  final commandRunner = CommandRunner('n-dart', 'NodeJS version manager')
     ..addCommand(InstallCommand())
     ..addCommand(UninstallCommand())
     ..addCommand(UseCommand())
     ..addCommand(ListCommand())
     ..addCommand(ListRemoteCommand())
-    ..addCommand(ReplaceNPMCommand())
-    ..run(args);
+    ..addCommand(ReplaceNPMCommand());
+
+  commandRunner.run(args).catchError((err) {
+    if (err is! UsageException) throw err as Object;
+
+    stdout.writeln(err);
+  });
 }
