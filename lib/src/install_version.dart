289ec1f73fcc4d49d03e5eb018963b91223d40c7
Jesus R <rr.jesus@pm.me>
2021-10-10 09:46:14 -0500

    Drop non-Windows support

--- lib/src/install_version.dart
+++ lib/src/install_version.dart
@@ -13,74 +13,43 @@ Future<void> installVersion(String version) async {
     return;
   }
 
-  var url = 'https://nodejs.org/dist/v';
-  final downloadName =
-      '${'node-v$version-'}${Platform.isWindows ? 'win' : Platform.isLinux ? 'linux' : Platform.isMacOS ? 'darwin' : ''}${'-${config.arch}'}';
-  final downloadExtension = Platform.isWindows ? 'zip' : 'tar.xz';
-
-  url += '$version/$downloadName.$downloadExtension';
+  const url = 'https://nodejs.org/dist/v';
+  final downloadName = 'node-v$version-win-${config.arch}';
+  const downloadExtension = 'zip';
 
   List<int> fileBytes;
 
-  // Download file
-  try {
-    fileBytes = await downloadFile(
-      url,
-      '$downloadName.$downloadExtension',
-      version,
-    );
-  } catch (e) {
-    stdout.writeln(e.toString());
-    exitCode = 2;
-    return;
+  fileBytes = await downloadFile(
+    '$url$version/$downloadName.$downloadExtension',
+    '$downloadName.$downloadExtension',
+    version,
+  );
+
+  stdout.writeln('Extracting file content');
+
+  final fileDecoder = ZipDecoder().decodeBytes(fileBytes);
+
+  for (final file in fileDecoder) {
+    if (file.isFile) {
+      File(
+        path.join(config.home, 'versions', file.name),
+      )
+        ..createSync(recursive: true)
+        ..writeAsBytesSync(file.content as List<int>);
+    } else {
+      Directory(
+        path.join(config.home, 'versions', file.name),
+      ).createSync(recursive: true);
+    }
   }
 
-  // Extract downloaded file
-  try {
-    stdout.writeln('Extracting file content');
-
-    Archive fileDecoder;
-
-    switch (downloadExtension) {
-      case 'zip':
-        fileDecoder = ZipDecoder().decodeBytes(fileBytes);
-        break;
-      case 'tar.xz':
-        fileDecoder = TarDecoder().decodeBytes(fileBytes);
-        break;
-      default:
-        stdout.write(
-          "Failed to extract contents. Error: file extension doesn't match",
-        );
-        return;
-    }
-
-    for (final file in fileDecoder) {
-      if (file.isFile) {
-        File(
-          path.join(config.home, 'versions', file.name),
-        )
-          ..createSync(recursive: true)
-          ..writeAsBytesSync(file.content as List<int>);
-      } else {
-        Directory(
-          path.join(config.home, 'versions', file.name),
-        ).createSync(recursive: true);
-      }
-    }
-
-    Directory(path.join(config.home, 'versions', downloadName)).renameSync(
-      path.join(config.home, 'versions', version),
-    );
-  } catch (e) {
-    stdout.writeln(e.toString());
-    exitCode = 2;
-    return;
-  }
+  Directory(path.join(config.home, 'versions', downloadName)).renameSync(
+    path.join(config.home, 'versions', version),
+  );
 
   config.installedVersions.add(version);
 
-  if (config.activeVersion == '') {
+  if (config.activeVersion.isEmpty) {
     stdout.writeln('Setting $version as active version');
     setAsActive(version);
   }
