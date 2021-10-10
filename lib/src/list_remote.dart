425bbe004665403ecaa06c8b60449c8db8553fd1
Jesus R <rr.jesus@pm.me>
2021-10-10 09:25:11 -0500

    feat: Show remote versions from bottom to top

--- lib/src/list_remote.dart
+++ lib/src/list_remote.dart
@@ -2,25 +2,27 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:n_dart/colors/colors.dart' as colors;
 
 Future<void> listRemote() async {
   const url = 'https://nodejs.org/dist/index.json';
   final request = await http.get(Uri.parse(url));
 
   if (request.statusCode != 200) {
-    stdout.writeln('Unexpected error fetching remote versions');
-    return;
+    throw Exception('Failed to fetch remote versions.');
   }
 
   stdout.writeln('Latest version of every major release (since 8):');
   final remoteVersions = json.decode(request.body) as List<dynamic>;
-  var major = -1;
+  int major = -1;
+
+  final versions = <String>[];
 
   for (final entry in remoteVersions) {
     final currentMajor =
         int.parse(entry['version'].substring(1).split('.')[0] as String);
 
-    if (currentMajor < 8) continue;
+    if (currentMajor < 8) break;
 
     if (currentMajor == major) {
       continue;
@@ -28,8 +30,14 @@ Future<void> listRemote() async {
 
     major = currentMajor;
     final isLTS = entry['lts'] != false;
-    stdout.writeln(
-      entry['version'].substring(1) + (isLTS ? '\x1b[34m - LTS \x1b[0m' : ''),
+
+    versions.add(
+      (entry['version'] as String).substring(1) +
+          (isLTS ? colors.green(' - LTS') : ''),
     );
+  }
+
+  for (final version in versions.reversed) {
+    stdout.writeln(version);
   }
 }
